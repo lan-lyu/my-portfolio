57c5c332f7c2fa891478cc9607906eb036367110
Alyssa14411 <40880366+Alyssa14411@users.noreply.github.com>
2020-07-25 20:54:41 +0800

    update self-portfolio

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,12 +12,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+
+function initMap() {
+  var highschool = {lat: 30.176387, lng:120.138887131};
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: highschool});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: highschool, map: map});
+}
+
 /**
  * Adds a random greeting to the page.
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['hello world', 'hahhahahah', '这波可以有','aaaaa','   试试','哈哈','你知道我是谁吗'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
@@ -25,4 +34,71 @@ function addRandomGreeting() {
   // Add it to the page.
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
+}
+/**
+ * Fetches a random quote from the server and adds it to the DOM.
+ */
+function getRandomQuote() {
+  console.log('Fetching a random quote.');
+
+  // The fetch() function returns a Promise because the request is asynchronous.
+  const responsePromise = fetch('/random-quote');
+
+  // When the request is complete, pass the response into handleResponse().
+  responsePromise.then(handleResponse);
+}
+
+/**
+ * Handles response by converting it to text and passing the result to
+ * addQuoteToDom().
+ */
+function handleResponse(response) {
+  console.log('Handling the response.');
+
+  // response.text() returns a Promise, because the response is a stream of
+  // content and not a simple variable.
+  const textPromise = response.text();
+
+  // When the response is converted to text, pass the result into the
+  // addQuoteToDom() function.
+  textPromise.then(addQuoteToDom);
+}
+
+/** Adds a random quote to the DOM. */
+function addQuoteToDom(quote) {
+  console.log('Adding quote to dom: ' + quote);
+
+  const quoteContainer = document.getElementById('quote-container');
+  quoteContainer.innerText = quote;
+}
+
+/**
+ * Fetches stats from the servers and adds them to the DOM.
+ 
+function getComments() {
+  fetch('/comments').then(response => response.json()).then((comments) => {
+    // stats is an object, not a string, so we have to
+    // reference its fields to create HTML content
+
+    const commentsListElement = document.getElementById('comments-container');
+    commentsListElement.innerHTML = '';
+    for(let i=0;i<comments.length;i++){
+        commentsListElement.appendChild(
+            createListElement(comments[i].comments));
+    }
+  });
+}
+*/
+
+async function getComments() {
+  const response = await fetch('/comments');
+  const quote = await response.text();
+  document.getElementById('quote-container').innerText = quote;
+}
+
+/** Creates an <li> element containing text. */
+function createListElement(text) {
+  const liElement = document.createElement('li');
+  liElement.innerText = text;
+  return liElement;
 }
